2ce7ee25907f193a0b75e112f75883e31327c9a9
Guillaume Beaud <beaudguillaume@gmail.com>
2016-04-11 21:15:18 +0200

    message

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
 
     name='linvpy',
 
-    version='0.1.537',
+    version='0.1.538',
 
     description='Package to solve linear inverse problems',
     long_description=long_description,
