b27572ff2ea14cf91fa1ba7dad0cb1d7a0de401b
Guillaume Beaud <beaudguillaume@gmail.com>
2016-05-15 17:55:50 +0200

    fixed doc issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ import os
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'scipy.special', 'math']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'scipy.special', 'math', '__future__', 'toolboxutilities', 'linvpy_toolbox']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
