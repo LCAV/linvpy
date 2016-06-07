8da5762b13383cd2db36cc516d3c41dd72965fe8
Marta Martinez-Camara <marta.martinez-camara@epfl.ch>
2016-06-07 17:50:10 +0200

    more little changes

--- linvpy.py
+++ linvpy.py
@@ -543,7 +543,7 @@ def irls(
     '''
     The method of iteratively reweighted least squares (IRLS) minimizes iteratively the function:
 
-    :math:`\\boldsymbol x^{(t+1)} = `
+    :math:`\\boldsymbol x^{(t+1)} =`
     :math:`{\\underset{\\boldsymbol x} \\operatorname{arg\,min}}`
     :math:`\\boldsymbol W (\\boldsymbol x^{(t)})\\big|\\big| \\boldsymbol y - \\boldsymbol A \\boldsymbol x \\big | \\big |_2^2.`
 
