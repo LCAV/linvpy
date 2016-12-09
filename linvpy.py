d67f93eb464d4694152698203e103fff4dceecc2
Guillaume Beaud <beaudguillaume@gmail.com>
2016-12-09 21:33:54 +0100

    message

--- linvpy.py
+++ linvpy.py
@@ -81,9 +81,8 @@ class Huber(LossFunction):
         :type array: numpy.ndarray
         :return: Array of same shape as the input, cell-wise results of the loss function
         :rtype: numpy.ndarray
-        :Examples:
-
-            >>> print(1+2)
+        :Example: >>> print(1+2)
+        >>> print(1+2)
 
         >>> import linvpy as lp
         >>> huber = lp.Huber()
