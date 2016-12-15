38ff14206628c96aba27dcf88367e06014ea9e61
Guillaume Beaud <beaudguillaume@gmail.com>
2016-12-15 11:13:08 +0100

    message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -201,6 +201,8 @@ html_show_sphinx = True
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 html_show_copyright = True
 
+viewcode_import = True
+
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
