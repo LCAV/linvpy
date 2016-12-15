5acecb5efc5c9b73f71aa553f186592956c8b37e
Guillaume Beaud <beaudguillaume@gmail.com>
2016-12-15 13:54:57 +0100

    message

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosummary',
 
-    'sphinx.contrib.autorun'
+    'sphinxcontrib.httpdomain'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
