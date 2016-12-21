574cf439324b13d2546b2b71bee59e2e0cf8e4ae
Guillaume Beaud <beaudguillaume@gmail.com>
2016-12-21 12:01:04 +0100

    message

--- setup.py
+++ setup.py
@@ -54,13 +54,13 @@ setup(
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    #packages=['linvpy'],
+    packages=['linvpy'],
 
-    packages=find_packages(exclude=['contrib', 'docs', 'tests/', 'dist']),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests/', 'dist']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #py_modules=['linvpy'],
+    py_modules=['linvpy'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
