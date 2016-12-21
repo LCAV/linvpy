43d3b4f2bf1296c85cc4576b879bd61a230807b6
Guillaume Beaud <beaudguillaume@gmail.com>
2016-12-21 12:03:27 +0100

    message

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    # py_modules=['linvpy'],
+    py_modules=['linvpy.py'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
