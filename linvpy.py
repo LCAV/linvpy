747912e73bc73571d846b22f79d6a327477b6e8b
GuillaumeBeaud <beaudguillaume@gmail.com>
2019-01-27 16:13:55 +0100

    Fixed documentation of Bisquare.psi function.

--- linvpy.py
+++ linvpy.py
@@ -263,8 +263,8 @@ class Bisquare(LossFunction):
         The derivative of bisquare loss (or Tukey's loss), "psi" version.
 
         :math:`\\psi(x)=\\begin{cases}
-        x((1-(\\frac{x}{c})^2)^2)& \\text{if |x|} \\leq 0 \\\\
-        0& \\text{if |x| > 0}
+        x((1-(\\frac{x}{c})^2)^2)& \\text{if |x|} \\leq c \\\\
+        0& \\text{if |x| > c}
         \\end{cases}`
 
         :param array: Array of values to apply the loss function to
